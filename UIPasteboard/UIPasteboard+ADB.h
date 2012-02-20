e8c600805770854000200df67d21f6558bde3e39
Alberto De Bortoli <albertodebortoli@gmail.com>
2012-02-20 23:24:08 +0100

    Add UIPasteboard+ADB

---
+++ UIPasteboard/UIPasteboard+ADB.h
@@ -0,0 +1,16 @@
+//
+//  UIPasteboard+ADB.h
+//  ADBCategories
+//
+//  Created by Alberto De Bortoli on 20/02/12.
+//  Copyright 2012 Alberto De Bortoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIPasteboard (ADB)
+
++ (void)copyURLToPasteboard:(NSString *)path;
++ (void)copyImageToPasteboard:(UIImage *)image;
+
+@end
---
+++ UIPasteboard/UIPasteboard+ADB.m
@@ -0,0 +1,23 @@
+//
+//  UIPasteboard+ADB.h
+//  ADBCategories
+//
+//  Created by Alberto De Bortoli on 20/02/12.
+//  Copyright 2012 Alberto De Bortoli. All rights reserved.
+//
+
+#import "UIPasteboard+ADB.h"
+
+@implementation UIPasteboard (ADB)
+
++ (void)copyURLToPasteboard:(NSString *)path
+{
+    [UIPasteboard generalPasteboard].string = path;
+}
+
++ (void)copyImageToPasteboard:(UIImage *)image
+{
+    [UIPasteboard generalPasteboard].image = image;
+}
+
+@end
