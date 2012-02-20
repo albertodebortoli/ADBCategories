a3499c8490691cbe5f69bcb846e7c1d7ef2423c4
Alberto De Bortoli <albertodebortoli@gmail.com>
2012-02-20 22:51:45 +0100

    Create project

---
+++ ADBCategories/ADBCategories/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ADBCategories
+//
+//  Created by Alberto De Bortoli on 20/02/12.
+//  Copyright 2012 Alberto De Bortoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
