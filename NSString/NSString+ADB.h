ebf8c96e8e26f3771e883282f84dbe6c7eb67e06
Alberto De Bortoli <albertodebortoli@gmail.com>
2012-03-15 22:12:27 +0100

    Add NSString+ADB

---
+++ NSString/NSString+ADB.h
@@ -0,0 +1,17 @@
+//
+//  NSString+ADB.h
+//  ADBCategories
+//
+//  Created by Alberto De Bortoli on 15/03/12.
+//  Copyright 2012 Alberto De Bortoli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (ADB)
+
++ (BOOL)isValidEmailString:(NSString *)email;
+- (BOOL)isNotEmpty;
+- (NSString *)urlEncodeUsingEncoding:(NSStringEncoding)encoding;
+
+@end
