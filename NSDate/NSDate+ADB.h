f0f338d3bd4aef05573465d97e14973f56465cdf
Alberto De Bortoli <albertodebortoli@gmail.com>
2012-03-15 22:17:17 +0100

    Add NSDate+ADB

---
+++ NSDate/NSDate+ADB.h
@@ -0,0 +1,16 @@
+//
+//  NSDate+ADB.h
+//  ADBCategories
+//
+//  Created by Alberto De Bortoli on 15/03/12.
+//  Copyright 2012 Alberto De Bortoli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (ADB)
+
++ (NSString *)getCurrentDateAsStringWithFormat:(NSString *)format;
+- (NSString *)dateWithFormat:(NSString *)format;
+
+@end
---
+++ NSDate/NSDate+ADB.m
@@ -0,0 +1,32 @@
+//
+//  NSDate+ADB.h
+//  ADBCategories
+//
+//  Created by Alberto De Bortoli on 15/03/12.
+//  Copyright 2012 Alberto De Bortoli. All rights reserved.
+//
+
+#import "NSDate+ADB.h"
+
+@implementation NSDate (ADB)
+
++ (NSString *)getCurrentDateAsStringWithFormat:(NSString *)format
+{
+    NSDate *chosenDate = [NSDate date];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:format];
+    NSString *date = [formatter stringFromDate:chosenDate];
+    
+    return date;
+}
+
+- (NSString *)dateWithFormat:(NSString *)format
+{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:format];
+    NSString *date = [formatter stringFromDate:self];
+    
+    return date;
+}
+
+@end
